0751eaae627816204bfa573fba437a52f73198e7
pigsye <eayancheong@gmail.com>
2025-05-19 17:05:09 +0800

    in case i forget to commit lmao

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,4 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 
