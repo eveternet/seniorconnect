0790211bafd2c8ffc437a46d80346bdd35dfd0ea
pigsye <eayancheong@gmail.com>
2025-05-20 16:01:38 +0800

    idek atp

--- src/components/interest/interest.jsx
+++ src/components/interest/interest.jsx
@@ -20,12 +20,37 @@ export default function () {
   }
 
   useEffect(() => {
-    fetch(API_BASE_URL() + "/interest_groups/info")
-      .then((response) => {
-        console.log(response);
-        response.json();
-      })
-      .then((data) => setInterestGroups(data));
+    setInterestGroups([
+      {
+        name: "Chess",
+        description:
+          "Play a fun game that keeps your brain sharp. Learn new moves!",
+      },
+      {
+        name: "Checkers",
+        description:
+          "A classic game! Easy to learn, fun to play. Use your thinking.",
+      },
+      {
+        name: "Mahjong",
+        description:
+          "Play with tiles! A classic game from far away. Good for your mind and friends.",
+      },
+      {
+        name: "Poker",
+        description:
+          "Play cards together! A fun game. Use your quick thinking. Just for fun!",
+      },
+      {
+        name: "Yoga",
+        description: "Gentle moves to feel good. Helps you bend and feel calm.",
+      },
+      {
+        name: "Photography",
+        description:
+          "Take pictures! Use your camera or phone. Show what you see.",
+      },
+    ]);
   }, []);
 
   return (
