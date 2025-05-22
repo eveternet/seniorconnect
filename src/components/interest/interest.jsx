3053f8c3c62de6c306e1d9cc9a8337ce6ee6fcef
pigsye <eayancheong@gmail.com>
2025-05-22 17:27:36 +0800

    fetch for once frfr??

--- src/components/interest/interest.jsx
+++ src/components/interest/interest.jsx
@@ -20,37 +20,13 @@ export default function () {
   }
 
   useEffect(() => {
-    setInterestGroups([
-      {
-        name: "Chess",
-        description:
-          "Play a fun game that keeps your brain sharp. Learn new moves!",
-      },
-      {
-        name: "Checkers",
-        description:
-          "A classic game! Easy to learn, fun to play. Use your thinking.",
-      },
-      {
-        name: "Mahjong",
-        description:
-          "Play with tiles! A classic game from far away. Good for your mind and friends.",
-      },
-      {
-        name: "Poker",
-        description:
-          "Play cards together! A fun game. Use your quick thinking. Just for fun!",
-      },
-      {
-        name: "Yoga",
-        description: "Gentle moves to feel good. Helps you bend and feel calm.",
-      },
-      {
-        name: "Photography",
-        description:
-          "Take pictures! Use your camera or phone. Show what you see.",
-      },
-    ]);
+    fetch("https://seniorconnect-python.vercel.app/api/interest_groups/info")
+      .then((data) => {
+        setInterestGroups(data.json());
+      })
+      .catch((error) => {
+        console.error("An error occurred during fetch or processing:", error);
+      });
   }, []);
 
   return (
