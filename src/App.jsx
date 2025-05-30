d237050a670889fd7274964a41a449c7c31bf7d4
pigsye <eayancheong@gmail.com>
2025-05-30 22:22:11 +0800

    oops i did it again

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import User_routes from "./components/user_routes/user_routes";
 import Auth from "./components/middleware/auth";
+import ScrollToTop from "./components/middleware/scroll";
 function App() {
     return (
         <>
             <div className="min-h-screen bg-blue-200">
                 <BrowserRouter>
+                    <ScrollToTop />
                     <Auth />
                     <Routes>
                         <Route path="/*" element={<User_routes />} />
