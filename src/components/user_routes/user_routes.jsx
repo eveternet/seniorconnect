da56951db84d36eb39a9ce7a962d7195bf1adbfd
pigsye <eayancheong@gmail.com>
2025-05-30 20:02:51 +0800

    how did i miss that

--- src/components/user_routes/user_routes.jsx
+++ src/components/user_routes/user_routes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import NotFound from "./404/notfound";
 import Home from "./home/home";
-import InterestGroups from "./interest_groups/routes";
+import InterestGroups from "./interest/routes";
 import SignIn from "./signin/signin";
 import SignUp from "./signin/signup";
 import Navbar from "./navbar/navbar"; // Assuming you have a Navbar
