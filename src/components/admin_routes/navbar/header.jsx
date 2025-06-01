7646eb244611b94798f10e7192a4086e5c34e839
pigsye <eayancheong@gmail.com>
2025-06-01 21:13:14 +0800

    a

--- src/components/admin_routes/navbar/header.jsx
+++ src/components/admin_routes/navbar/header.jsx
@@ -7,49 +7,49 @@ export default function Navbar() {
             <div className="flex h-screen w-60 flex-col bg-blue-900">
                 <Link
                     to="/admin"
-                    className="mx-5 cursor-pointer pt-5 text-left text-base text-blue-100"
+                    className="mx-5 cursor-pointer py-3 pt-5 text-left text-base text-blue-100"
                 >
                     {" "}
                     SeniorConnect{" "}
                 </Link>
                 <Link
                     to="/"
-                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 text-left text-base text-blue-100"
+                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 py-3 text-left text-base text-blue-100"
                 >
                     {" "}
                     Return to users page{" "}
                 </Link>
                 <Link
                     to="/admin/users"
-                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 text-left text-base text-blue-100"
+                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 py-3 text-left text-base text-blue-100"
                 >
                     {" "}
                     Users{" "}
                 </Link>
                 <Link
                     to="/admin/interest-groups"
-                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 text-left text-base text-blue-100"
+                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 py-3 text-left text-base text-blue-100"
                 >
                     {" "}
                     Interest Groups{" "}
                 </Link>
                 <Link
                     to="/admin/events"
-                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 text-left text-base text-blue-100"
+                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 py-3 text-left text-base text-blue-100"
                 >
                     {" "}
                     Events{" "}
                 </Link>
                 <Link
                     to="/admin/chat"
-                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 text-left text-base text-blue-100"
+                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 py-3 text-left text-base text-blue-100"
                 >
                     {" "}
                     Chat{" "}
                 </Link>
                 <Link
                     to="/admin/notifications"
-                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 text-left text-base text-blue-100"
+                    className="mx-5 cursor-pointer border-t border-solid border-blue-100 py-3 text-left text-base text-blue-100"
                 >
                     {" "}
                     Notifications{" "}
