01887d3da108a0fc0ffdcc91e9b2888feca3c2f1
eveternet <eveternet@gmail.com>
2025-06-01 22:05:06 +0800

    a

--- src/components/admin_routes/interest_group/route.jsx
+++ src/components/admin_routes/interest_group/route.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Listings from "./listing/listing";
 import Edit from "./edit/edit";
+import InterestGroupApplicationsAdmin from "./applications/applications";
 
 export default function InterestGroupsRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Listings />} />
             <Route path="/:id" element={<Edit />} />
+            <Route
+                path="/applications"
+                element={<InterestGroupApplicationsAdmin />}
+            />
         </Routes>
     );
 }
